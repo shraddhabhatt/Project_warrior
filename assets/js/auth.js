e1ef1b93a43bf74d2c37ffb7108d183bad7dedc4
shraddhabhatt <trivedishraddha99@gmail.com>
2018-02-03 01:36:49 -0500

    commit includes new signup/signin pages

---
+++ assets/js/auth.js
@@ -0,0 +1,76 @@
+$(document).ready(function(){
+	
+	var config = {
+	    apiKey: "AIzaSyBJEf-9pGFWooqFdxd-sLAkozFU-YV369M",
+	    authDomain: "project-weeken-warrior.firebaseapp.com",
+	    databaseURL: "https://project-weeken-warrior.firebaseio.com",
+	    projectId: "project-weeken-warrior",
+	    storageBucket: "project-weeken-warrior.appspot.com",
+	    messagingSenderId: "11980125710"
+	  };
+	    
+	firebase.initializeApp(config);
+
+	// Create a variable to reference the database
+	var database = firebase.database();
+
+	$("#signup-form").validate({
+		errorClass: 'invalid',
+             errorPlacement: function (error, element) {
+        $(element)
+            .closest("form")
+            .find("label[for='" + element.attr("id") + "']")
+            .attr('data-error', error.text());
+    	},
+	    // Specify validation rules
+	    rules: {
+	      firstname: "required",
+	      lastname: "required",
+	      email: {
+	        required: true,
+	        email: true
+	      },
+	      password: {
+	        required: true,
+	        minlength: 5
+	      },
+	      confirm_password: {
+	         required: true,
+	         minlength: 6,
+	         equalTo: "#password"
+	       }
+	    },
+	    // Specify validation error messages
+	    messages: {
+	      firstname: "Please enter your firstname",
+	      lastname: "Please enter your lastname",
+	      password: {
+	        required: "Please provide a password",
+	        minlength: "Your password must be at least 6 characters long"
+	      },
+	      confirm_password: {
+	        required: "Please provide a password",
+	        minlength: "Your password must be at least 6 characters long",
+	        equalTo: "Password doesn't match"
+	      },
+	      email: "Please enter a valid email address"
+	    },
+	    // Make sure the form is submitted to the destination defined
+	    // in the "action" attribute of the form when valid
+	    submitHandler: function(form) {
+	      form.submit();
+	      signup();
+	    }
+	  });
+
+	function signup(){
+		
+		let email = $("#email").val().trim();
+		let password = $("#password").val().trim();
+
+		firebase.auth().createUserWithEmailAndPassword(email, password)
+ 					.catch(function (err) {
+   					// Handle errors
+ 		});
+	}
+});
