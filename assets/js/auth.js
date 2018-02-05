b41685e0c7db103980f3e0d1fe31ceddd2e129fd
shraddhabhatt <trivedishraddha99@gmail.com>
2018-02-04 19:40:21 -0500

    updated signup/signin

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -123,17 +123,7 @@ function signup(){
  		}
  		let email_signin = $("#email-signin").val().trim();
 		let password_signin = $("#password-signin").val().trim();
- 		firebase.auth().signInWithEmailAndPassword(email_signin, password_signin).then(function(result){
-			
-			console.log("inside connect 1"+result);
-
-			user = firebase.auth().currentUser;
- 		
-			console.log("current user :: "+user);
-			//clearform();
-			//window.location.href = "options.html";
-
- 		}).catch(function(error) {
+ 		firebase.auth().signInWithEmailAndPassword(email_signin, password_signin).catch(function(error) {
 			  // Handle Errors here.
 			  var errorCode = error.code;
 			  var errorMessage = error.message;
@@ -145,6 +135,15 @@ function signup(){
 	  		 console.log("ERRORS :: "+ errorCode);
 			
 			});
+ 		firebase.auth().onAuthStateChanged(function(user) {
+  		if (user!=null) {
+    		// User is signed in.
+    		console.log("auth changed :: "+user);
+  		} else {
+    		// No user is signed in.
+    		console.log("auth changed NO:: "+user);
+  		}
+	});
  });
  $("#googlesignin").on("click", function(){
 
@@ -173,4 +172,25 @@ function signup(){
 			
 			});
 	});
- 	
+ 	$("#fbsignin").on("click", function() 	{
+
+ 		var provider = new firebase.auth.FacebookAuthProvider();
+ 		firebase.auth().signInWithPopup(provider).then(function(result) {
+  		// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  		var token = result.credential.accessToken;
+  		// The signed-in user info.
+  		var user = result.user;
+  		console.log("FB Login :: "+user);
+  		// ...
+		}).catch(function(error) {
+  		// Handle Errors here.
+  		var errorCode = error.code;
+  		var errorMessage = error.message;
+  		// The email of the user's account used.
+  		var email = error.email;
+  		// The firebase.auth.AuthCredential type that was used.
+  		var credential = error.credential;
+  		// ...
+  		console.log("FB Erorr:: ":errorMessage);
+		});
+ 	})
